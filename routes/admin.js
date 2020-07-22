70f3e5405fa21764f775d15fe789003adee0c02c
MXmaster2s <bullzeye711@gmail.com>
2020-07-22 18:31:59 +0530

    minor changes

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({ 
+    title: req.body.title 
+  });
   res.redirect('/');
 });
 
