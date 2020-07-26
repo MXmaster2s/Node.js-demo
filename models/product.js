18e92688da4541d1ff1cf85cc14a2afe7a4f487a
MXmaster2s <bullzeye711@gmail.com>
2020-07-26 19:32:50 +0530

    re-structured controllers and added new views

--- models/product.js
+++ models/product.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { getProducts } = require('../controllers/products');
 
 const products = [];        
 
@@ -21,8 +20,11 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
